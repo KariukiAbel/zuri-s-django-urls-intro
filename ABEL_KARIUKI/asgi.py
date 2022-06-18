6825c437d182b92985b1161aeadad34e965d2a35
KariukiAbel <abelmuriithi@gmail.com>
2022-06-18 09:47:11 +0300

    Zuri's I4G scholarship test on django urls

---
+++ ABEL_KARIUKI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ABEL_KARIUKI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ABEL_KARIUKI.settings')
+
+application = get_asgi_application()
